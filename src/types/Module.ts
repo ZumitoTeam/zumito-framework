9048f3e330d824db2db23c2c71d03ee0761c9532
Fernando Merino <fernandomema@gmail.com>
2022-12-04 13:57:36 +0100

    Load translations from subfolders too

--- src/types/Module.ts
+++ src/types/Module.ts
@@ -158,24 +158,33 @@ export abstract class Module {
         return this.events;
     }
 
-    async registerTranslations() {
-        if (!fs.existsSync(path.join(this.path, 'translations'))) return;
-        let files = fs.readdirSync(path.join(this.path, 'translations'));
+    async registerTranslations(subpath: string = '') {
+        if (!fs.existsSync(path.join(this.path, 'translations', subpath))) return;
+        let files = fs.readdirSync(path.join(this.path, 'translations', subpath));
         for (let file of files) {
             if (file.endsWith('.json')) {
-                let json = await import('file://' + `${this.path}/translations/${file}`, {
-                    assert: {
-                        type: "json",
-                    },
-                }).catch(e => {
-                    console.error(`[🔄🔴 ] Error loading ${file.slice(0, -5)} translations on module ${this.constructor.name}`);
-                    console.log(boxen(e + '\n' + e.name + '\n' + e.stack, { padding: 1 }));
-                });
+                let json = await this.loadTranslationFile(subpath, file);
                 let lang = file.slice(0, -5);
-                this.parseTranslation('', lang,  json.default);
+                subpath = subpath ? subpath.replaceAll('/', '.') + '.' : '';
+                this.parseTranslation(subpath, lang,  json);
+            } else if (fs.lstatSync(path.join(this.path, 'translations', file)).isDirectory()) {
+                await this.registerTranslations(path.join(subpath, file));
             }
         }
-    }       
+    }
+    
+    async loadTranslationFile(subpath: string, file: string) {
+        if (subpath) subpath = subpath + '/';
+        let json = await import('file://' + `${this.path}/translations/${subpath}${file}`, {
+            assert: {
+                type: "json",
+            },
+        }).catch(e => {
+            console.error(`[🔄🔴 ] Error loading ${file.slice(0, -5)} translations on module ${this.constructor.name}`);
+            console.error(e + '\n' + e.name + '\n' + e.stack);
+        });
+        return json.default;
+    }
 
     parseTranslation(path: string, lang: string, json: any): any {
         if (typeof json === 'object') {
