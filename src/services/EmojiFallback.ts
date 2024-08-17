7f2b251de8e7eda9d187b7c16a7bad0117da9e7e
fernandomema <fernandomema@gmail.com>
2024-08-17 17:17:49 +0200

    Convert emoji fallback to service

--- src/services/EmojiFallback.ts
+++ src/services/EmojiFallback.ts
@@ -1,32 +1,45 @@
 import { Client, Emoji } from 'discord.js';
+import { TranslationManager } from './TranslationManager';
 
 export class EmojiFallback {
-    public static getEmoji(
-        client: Client,
+
+    client: Client;
+    translator: TranslationManager;
+
+    constructor(client: Client, translator: TranslationManager) {
+        this.client = client;
+        this.translator = translator;
+    }
+
+    public getEmoji(
         emojiId: string,
         fallbackEmoji: any
     ) {
-        const emoji = client.emojis.cache.get(emojiId);
+        if (this.translator.has(emojiId)) {
+            emojiId = this.translator.get(emojiId);
+        }
+        const emoji = this.client.emojis.cache.get(emojiId);
         return emoji?.toString() || fallbackEmoji;
     }
 
-    public static getEmojiByName(
-        client: Client,
+    public getEmojiByName(
         emojiName: string,
         fallbackEmoji: any
     ) {
-        const emoji = client.emojis.cache.find(
+        const emoji = this.client.emojis.cache.find(
             (emoji) => emoji.name === emojiName
         );
         return emoji?.toString() || fallbackEmoji;
     }
 
-    public static getEmojiByIdentifier(
-        client: Client,
+    public getEmojiByIdentifier(
         emojiId: string,
         fallbackEmoji: any
     ) {
-        const emoji = client.emojis.cache.find((emoji) => emoji.id === emojiId);
+        if (this.translator.has(emojiId)) {
+            emojiId = this.translator.get(emojiId);
+        }
+        const emoji = this.client.emojis.cache.find((emoji) => emoji.id === emojiId);
         return emoji?.toString() || fallbackEmoji;
     }
 }
